0cec38784465b7a615d4255f593fe3192e5bfdea
sl <s.leyral@gmail.com>
2020-05-12 16:24:10 +0200

    DAL - CategorieDAO

---
+++ src/fr/eni/same/dal/jdbc/CategorieDAO.java
@@ -0,0 +1,41 @@
+package fr.eni.same.dal.jdbc;
+
+import java.util.List;
+
+import fr.eni.same.bo.Categorie;
+import fr.eni.same.dal.DAO;
+import fr.eni.same.exception.BusinessException;
+
+public class CategorieDAO implements DAO<Categorie> {
+
+	@Override
+	public void insert(Categorie t) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Categorie t) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Categorie t) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Categorie select(int id) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Categorie> selectAll() throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
