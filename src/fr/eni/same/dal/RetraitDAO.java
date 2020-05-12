b9bc1d61257168a9ca763c8b3beb6c50dc0f44d2
sl <s.leyral@gmail.com>
2020-05-12 22:11:38 +0200

    implement methode test RetraitDAO

--- src/fr/eni/same/dal/RetraitDAO.java
+++ src/fr/eni/same/dal/RetraitDAO.java
@@ -9,4 +9,47 @@ import fr.eni.same.bo.Vente;
 public interface RetraitDAO extends DAO<Vente> {
 
 	
+	/**
+	 * Verifie si le libelle fait moins de 30 caracteres
+	 * @param libelle
+	 * @return
+	 */
+	public boolean venteVerifRueLongeurMax(String libelle);
+	
+	/**
+	 * Verifie si le libelle fait plus de 4 caracteres
+	 * @param libelle
+	 * @return
+	 */
+	public boolean venteVerifRueLongeurMin(String libelle);
+	
+	/**
+	 * Verifie si le libelle fait 5 caracteres MAX
+	 * @param libelle
+	 * @return
+	 */
+	public boolean venteVerifCPLongeurMax(String libelle);
+	
+	
+	/**
+	 * Verifie si le libelle fait moins de 5 caracteres
+	 * @param libelle
+	 * @return
+	 */
+	public boolean venteVerifCPLongeurMin(String libelle);
+	/**
+	 * Verifie si le libelle fait moins de 30 caracteres
+	 * @param libelle
+	 * @return
+	 */
+	public boolean venteVerifVilleLongeurMax(String libelle);
+	
+	/**
+	 * Verifie si le libelle fait plus de 1 caracteres et different de ""
+	 * @param libelle
+	 * @return
+	 */
+	public boolean venteVerifVilleLongeurMin(String libelle);
+	
+	
 }
