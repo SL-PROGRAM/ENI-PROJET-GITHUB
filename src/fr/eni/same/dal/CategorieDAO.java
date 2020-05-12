941e756e01ae57cb95e06d4979d638d6ce98ff1d
Etienne <etienne.fdarcier@hotmail.fr>
2020-05-12 19:06:26 +0200

    Interfaces DAO
    Intégration des interfaces DAO dans le projet

---
+++ src/fr/eni/same/dal/CategorieDAO.java
@@ -0,0 +1,11 @@
+package fr.eni.same.dal;
+
+import fr.eni.same.bo.Categorie;
+
+/**
+ * Interface contenant les informations spécifiques à implémenter pour les catégories
+ * @author etienne
+ */
+public interface CategorieDAO extends DAO<Categorie> {
+	
+}
