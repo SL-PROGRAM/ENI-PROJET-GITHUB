e90bbc7b8bde0a8c34bc6c08d508854e03f1fa20
sl <s.leyral@gmail.com>
2020-05-12 23:16:56 +0200

    Creation class FonctionGeneriqueDAO
    contient les methodes statics a utiliser pour les tests

---
+++ src/fr/eni/same/dal/FonctionGeneriqueDAO.java
@@ -0,0 +1,23 @@
+package fr.eni.same.dal;
+
+public class LongeurStringDAO {
+
+
+	public static boolean isLongeurMax(String stingAtest, int longeurMax) {
+		boolean isUnique = true;
+		if(stingAtest.length() < longeurMax) {
+			isUnique = false;
+		}
+		return isUnique;
+	}
+	
+
+	public static boolean isLongeurMin(String stingAtest, int longeurMin) {
+		boolean isUnique = true;
+		if(stingAtest.length() > longeurMin) {
+			isUnique = false;
+		}
+		return isUnique;
+	}
+
+}
