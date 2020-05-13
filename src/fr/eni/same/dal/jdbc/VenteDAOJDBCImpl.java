119c1d98d19b04da5af046c0e869c58af4f29f48
Etienne <etienne.fdarcier@hotmail.fr>
2020-05-13 14:57:08 +0200

    Ajout timestamp - Vente DAOJDBCImpl
    Ajout du timestamp sur l'impémentation jdbc

--- src/fr/eni/same/dal/jdbc/VenteDAOJDBCImpl.java
+++ src/fr/eni/same/dal/jdbc/VenteDAOJDBCImpl.java
@@ -2,10 +2,11 @@ package fr.eni.same.dal.jdbc;
 
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.same.bo.Vente;
@@ -17,7 +18,6 @@ public class VenteDAOJDBCImpl implements VenteDAO{
 	/**
 	 * mise en place d'un singleton pour garantir la cohérence des données
 	 */
-	
 	private static VenteDAOJDBCImpl instance;
 
 	/**
@@ -53,7 +53,7 @@ public class VenteDAOJDBCImpl implements VenteDAO{
 			stmt.setInt(1, 0);
 			stmt.setString(2, t.getNomArticle());
 			stmt.setString(3, t.getDescription());
-			stmt.setDate(4, Date.valueOf(t.getDateFinEncheres()));
+			stmt.setTimestamp(4, t.getDateFinEncheres());
 			stmt.setInt(5, t.getMiseAPrix());
 			stmt.setInt(6, t.getPrixVente());
 			stmt.setInt(7, t.getUtilisateurVendeur().getNoUtilisateur());
@@ -76,7 +76,7 @@ public class VenteDAOJDBCImpl implements VenteDAO{
 			PreparedStatement stmt = con.prepareStatement(UPDATE);
 			stmt.setString(1, t.getNomArticle());
 			stmt.setString(2, t.getDescription());
-			stmt.setDate(3, Date.valueOf(t.getDateFinEncheres()));
+			stmt.setTimestamp(3, t.getDateFinEncheres());
 			stmt.setInt(4, t.getMiseAPrix());
 			stmt.setInt(5, t.getPrixVente());
 			stmt.setInt(6, t.getUtilisateurVendeur().getNoUtilisateur());
@@ -91,20 +91,69 @@ public class VenteDAOJDBCImpl implements VenteDAO{
 
 	@Override
 	public void delete(Vente t) throws BusinessException {
-		// TODO Auto-generated method stub
-		
+		Connection con = null;
+		con = ConnectionProvider.openConnection();
+		try {
+			PreparedStatement stmt = con.prepareStatement(DELETE);
+			stmt.setInt(1, t.getNoVente());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con = ConnectionProvider.closeConnection();
 	}
 
 	@Override
 	public Vente select(int id) throws BusinessException {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		Vente _vente = new Vente();
+		con = ConnectionProvider.openConnection();
+		try {
+			PreparedStatement stmt = con.prepareStatement(SELECT_BY_ID);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				_vente.setNoVente(rs.getInt(1));
+				_vente.setNomArticle(rs.getString(2));
+				_vente.setDescription(rs.getString(3));
+				_vente.setDateFinEncheres(rs.getTimestamp(4));
+				_vente.setMiseAPrix(rs.getInt(5));
+				_vente.setPrixVente(rs.getInt(6));
+//				_vente.setUtilisateurVendeur(rs.getInt(7));
+//				_vente.setCategorie(rs.getInt(8));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public List<Vente> selectAll() throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		con = ConnectionProvider.openConnection();
+		List<Vente> _venteList = new ArrayList<Vente>();
+		try {
+			PreparedStatement stmt = con.prepareStatement(SELECT_ALL);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+//				Vente _vente = new Vente(rs.getInt(1),
+//										rs.getString(2),
+//										rs.getString(3),
+//										rs.getDate(4),
+//										rs.getInt(5),
+//										rs.getInt(6),
+//										rs.getInt(7),
+//										rs.getInt(8));
+//				_venteList.add(_vente);
+//				System.out.println(_vente.toString());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con = ConnectionProvider.closeConnection();
+		return _venteList;
 	}
 
 
