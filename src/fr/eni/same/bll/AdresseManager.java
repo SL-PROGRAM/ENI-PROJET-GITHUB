e1c0edaf0961758c0eea25ce200fe0e0cb08edbd
sl <s.leyral@gmail.com>
2020-05-13 08:35:17 +0200

    Add AdresseManager pour refactor methode test commun Utilisateur et Retrait

---
+++ src/fr/eni/same/bll/AdresseManager.java
@@ -0,0 +1,31 @@
+package fr.eni.same.bll;
+
+import fr.eni.same.bll.interfaceManager.AdresseInterface;
+
+public class AdresseManager implements AdresseInterface {
+	private final int RUE_LONGEUR_MAX = 30;
+	private final int RUE_LONGEUR_MIN = 4;
+	private final int CP_LONGEUR_MAX = 5;
+	private final int CP_LONGEUR_MIN = 5;
+	private final int VILLE_LONGEUR_MAX = 30;
+	private final int VILLE_LONGEUR_MIN = 4;
+	
+	@Override
+	public boolean isRueLongeurCorrect(String libelle) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCodePostalLongeurCorrect(String libelle) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isVilleLongeurCorrect(String libelle) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
