708b9a4c9b2298c6eb475679a083b971980bd2eb
Loisel <loiselmathieu35@gmail.Com>
2020-05-13 16:43:58 +0200

    Création FiltreAutresEncheres
    Création FiltreAutresEncheres pour les filtres de la jsp listeEncheres.jsp

---
+++ src/fr/eni/same/filtre/FiltreAutresEncheres.java
@@ -0,0 +1,50 @@
+package fr.eni.same.filtre;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class FiltreAutresEncheres
+ */
+@WebFilter("/FiltreAutresEncheres")
+public class FiltreAutresEncheres implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FiltreAutresEncheres() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
