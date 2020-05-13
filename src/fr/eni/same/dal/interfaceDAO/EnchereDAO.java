e1d992235fa504916feffb82470a0ae626fdebef
sl <s.leyral@gmail.com>
2020-05-13 21:33:56 +0200

    ajout slect avec deux parametre pour gestion PK deux champs

--- src/fr/eni/same/dal/interfaceDAO/EnchereDAO.java
+++ src/fr/eni/same/dal/interfaceDAO/EnchereDAO.java
@@ -1,15 +1,17 @@
 package fr.eni.same.dal.interfaceDAO;
 
-import java.util.List;
 
-import fr.eni.same.bo.Categorie;
 import fr.eni.same.bo.Enchere;
+import fr.eni.same.exception.BusinessException;
 
 /**
  * Interface contenant les informations spécifiques à implémenter pour les enchères
  * @author etienne
  */
 public interface EnchereDAO extends DAO<Enchere> {
+
+	Enchere select(int noUtlisateur, int noVente) throws BusinessException;
+
 	
 	/**
 	 * Pas d'implementation
