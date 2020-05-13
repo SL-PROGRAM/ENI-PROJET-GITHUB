42ae625fbd7af56e2e9758d07c82cd259b280b42
Etienne <etienne.fdarcier@hotmail.fr>
2020-05-13 11:42:01 +0200

    MAJ - Fichiers de tests ServletTestConnexion
    Servlet de test de connexion mise à jour avec la fonction select by id

--- src/fr/eni/same/tests/ServletTestConnexion.java
+++ src/fr/eni/same/tests/ServletTestConnexion.java
@@ -27,11 +27,11 @@ public class ServletTestConnexion extends HttpServlet {
 		/**
 		 * Test de remplissage de la table utilisateurs avec tous les constructeurs différents
 		 */
-		Utilisateur standardA = new Utilisateur(19, "aArtiste","Artiste","alain","a@laposte.net","0656467616","rue","35000","Rennes","123",155,false);
-		Utilisateur standardB = new Utilisateur(20, "bBoule","Boule","bill","b@wanadoo.fr","0546876548","rue","44000","Nantes","456",200,false);
+		Utilisateur standardA = new Utilisateur(1, "aArtiste","Artiste","alain","a@laposte.net","0656467616","rue","35000","Rennes","123",155,false);
+		Utilisateur standardB = new Utilisateur("bBoule","Boule","bill","b@wanadoo.fr","0546876548","rue","44000","Nantes","456",200,false);
 		Utilisateur constructeurSansTel = new Utilisateur(21, "cCailloux","Cailloux","console","c@hotmail.fr","rue","75000","Paris","789",845,false);
-		Utilisateur constructeurSansPK = new Utilisateur(22,"dDouble","Double","gras","d@gmail.com","0645789876","avenue des tulipes","88465","Atlantide","4496",800,false);
-		Utilisateur constructeurSansPKSansTel = new Utilisateur(23,"eEtienne","Etoile","plop","etienne@hotmail.fr","rue","35000","Marseille","13000",12000,true);
+		Utilisateur constructeurSansPK = new Utilisateur("dDouble","Double","gras","d@gmail.com","0645789876","avenue des tulipes","88465","Atlantide","4496",800,false);
+		Utilisateur constructeurSansPKSansTel = new Utilisateur("eEtienne","Etoile","plop","etienne@hotmail.fr","rue","35000","Marseille","13000",12000,true);
 		
 		try {
 			DALFactory.getUtilisateurDAOJdbcImpl().insert(standardA);
@@ -43,6 +43,7 @@ public class ServletTestConnexion extends HttpServlet {
 			standardA.setTelephone("0123456789");
 			DALFactory.getUtilisateurDAOJdbcImpl().update(standardA);
 			DALFactory.getUtilisateurDAOJdbcImpl().delete(standardB);
+			Utilisateur u = DALFactory.getUtilisateurDAOJdbcImpl().select(4);
 		} catch (BusinessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
