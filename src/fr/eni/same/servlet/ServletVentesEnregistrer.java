555f08d90c52d3016f75b74b49f7d6e149727829
Loisel <loiselmathieu35@gmail.Com>
2020-05-13 14:08:06 +0200

    MAJ ServletVentesEnregistrees (dont modification nom de fichier)
    Ajout des commentaires doXXX + harmonisation des nommages entre servlet et jsp

---
+++ src/fr/eni/same/servlet/ServletVentesEnregistrer.java
@@ -0,0 +1,38 @@
+package fr.eni.same.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletVentesEnregistrees
+ */
+@WebServlet("/ServletVentesEnregistrer")
+public class ServletVentesEnregistrer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * doGet : redirection vers la page permettant d'afficher la liste des ventes enregistrées (ventesEnregistrer.jsp)
+	 * doPost : Récupération des informations de l'utilisateur, notamment la liste des ventes enegistrées et non finalisées de l'utilsiateur
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/ventesEnregistrer.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+
+}
+
