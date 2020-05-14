510ec1d15d7220bd0cfd4dbbd441172ca71fc752
Loisel <loiselmathieu35@gmail.Com>
2020-05-14 15:38:03 +0200

    Maj commentaires ServletEncherir
    Ajout des maquettes correspondantes

---
+++ src/fr/eni/same/ihm/servlet/ServletEncherir.java
@@ -0,0 +1,39 @@
+package fr.eni.same.ihm.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEnchere
+ */
+@WebServlet("/ServletEncherir")
+public class ServletEncherir extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * doGet : redirection vers la page permettant d'enchérir sur une vente (pageEncherir.jsp)
+	 * doPost : Récupération des informations liées à la vente sur laquelle je veux enchérir.
+	 * 
+	 *  Cette Servlet et la jsp correspondante prennent en charge les Maquettes 6 et 7
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/pageEncherir.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+
+}
