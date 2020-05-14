74a55b15cd2b91fd8d403cd4848f58e8df0ffb10
Loisel <loiselmathieu35@gmail.Com>
2020-05-14 15:37:51 +0200

    Maj Commentaires ServletEnchereGagnee
    Ajout des maquettes correspondantes

---
+++ src/fr/eni/same/ihm/servlet/ServletEnchereGagnee.java
@@ -0,0 +1,40 @@
+package fr.eni.same.ihm.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEnchereGagnee
+ */
+@WebServlet("/ServletEnchereGagnee")
+public class ServletEnchereGagnee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * doGet : redirection vers la page affichant les informations de l'enchère gagnée (enchereGagnee.jsp)
+	 * doPost : Récupération des informations liées à la vente que l'utilisateur a remporté
+	 * 
+	 *  Cette Servlet et la jsp correspondante prennent en charge la Maquette 8
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/enchereGagnee.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	
+}
+
