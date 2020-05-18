86adc5d6c835655be0d67741ef16f84a9e02f08b
sl <s.leyral@gmail.com>
2020-05-18 10:00:47 +0200

    AutoClose Enchere

--- src/fr/eni/same/dal/jdbc/EnchereDAOJDBCImpl.java
+++ src/fr/eni/same/dal/jdbc/EnchereDAOJDBCImpl.java
@@ -56,202 +56,182 @@ public class EnchereDAOJDBCImpl implements EnchereDAO {
 	
 	@Override
 	public void insert(Enchere t) throws DALException {
-		Connection con = ConnectionProvider.openConnection();
-		try {
-			PreparedStatement pstmt = con.prepareStatement(INSERT);
-			pstmt.setTimestamp(1, t.getDateEnchere());
-			pstmt.setInt(2, t.getUtilisateurEnchere().getNoUtilisateur());
-			pstmt.setInt(3, t.getVenteEnchere().getNoVente());
-			pstmt.execute();
-			pstmt.close();
-//			System.out.println("Enchere insérée en base de donnée : " + t.toString());
+		try(Connection con = ConnectionProvider.openConnection()) {
+			try(PreparedStatement pstmt = con.prepareStatement(INSERT)){
+				pstmt.setTimestamp(1, t.getDateEnchere());
+				pstmt.setInt(2, t.getUtilisateurEnchere().getNoUtilisateur());
+				pstmt.setInt(3, t.getVenteEnchere().getNoVente());
+				pstmt.execute();
+	//			System.out.println("Enchere insérée en base de donnée : " + t.toString());
+			}			
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			con = ConnectionProvider.closeConnection();		
+			throw new DALException("Erreur insert");
 		}
 	}
 
 	@Override
 	public void update(Enchere t) throws DALException {
-		Connection con = ConnectionProvider.openConnection();
-		try {
-			PreparedStatement pstmt = con.prepareStatement(UPDATE);
-			pstmt.setTimestamp(1, t.getDateEnchere());
-			pstmt.setInt(2, t.getUtilisateurEnchere().getNoUtilisateur());
-			pstmt.setInt(3, t.getVenteEnchere().getNoVente());
-			pstmt.executeUpdate();
-//			System.out.println("Update réalisée sur l'enchere : " + t.toString());
-			pstmt.close();
+		try(Connection con = ConnectionProvider.openConnection()) {
+			try(PreparedStatement pstmt = con.prepareStatement(UPDATE)){
+				pstmt.setTimestamp(1, t.getDateEnchere());
+				pstmt.setInt(2, t.getUtilisateurEnchere().getNoUtilisateur());
+				pstmt.setInt(3, t.getVenteEnchere().getNoVente());
+				pstmt.executeUpdate();
+	//			System.out.println("Update réalisée sur l'enchere : " + t.toString());
+			}		
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			con = ConnectionProvider.closeConnection();		
+			throw new DALException("Erreur update");
 		}
 	}
 
 	@Override
 	public void delete(Enchere t) throws DALException {
-		Connection con = ConnectionProvider.openConnection();
-		try {
-			PreparedStatement stmt = con.prepareStatement(DELETE);
-			stmt.setInt(1, t.getUtilisateurEnchere().getNoUtilisateur());		
+		try(Connection con = ConnectionProvider.openConnection()) {
+			try(PreparedStatement stmt = con.prepareStatement(DELETE)){
+				stmt.setInt(1, t.getUtilisateurEnchere().getNoUtilisateur());		
 			stmt.setInt(2, t.getVenteEnchere().getNoVente());	
 			stmt.execute();
 //			System.out.println("Enchere: user : " + t.getUtilisateurEnchere().getNoUtilisateur()+ " "
 //					+ "et vente : " + t.getVenteEnchere().getNoVente()+ " supprimé en base de donnée");
-			stmt.close();
-			} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			con = ConnectionProvider.closeConnection();		
+			}
+		} catch (SQLException e) {
+			throw new DALException("Erreur delete");
 		}
 	}
 
 	@Override
 	public Enchere select(int noUtlisateur, int noVente) throws DALException {
-		Connection con = ConnectionProvider.openConnection();
 		Enchere enchere=new Enchere();
-		try {
-			PreparedStatement pstmt = con.prepareStatement(SELECT_BY_ID, PreparedStatement.RETURN_GENERATED_KEYS);
-			pstmt.setInt(1, noUtlisateur);
-			pstmt.setInt(2, noVente);
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {		
-				
-				Utilisateur acheteur = new Utilisateur();
-				acheteur.setNoUtilisateur(rs.getInt(4));
-				acheteur.setPseudo(rs.getString(5));
-				acheteur.setNom(rs.getString(6));
-				acheteur.setPrenom(rs.getString(7));
-				acheteur.setEmail(rs.getString(8));
-				acheteur.setTelephone(rs.getString(9));
-				acheteur.setRue(rs.getString(10));
-				acheteur.setCodePostal(rs.getString(11));
-				acheteur.setVille(rs.getString(12));
-				acheteur.setMotDePasse(rs.getString(13));
-				acheteur.setCredit(rs.getInt(14));
-				acheteur.setAdministrateur(rs.getBoolean(15));
-				
-				Vente vente = new Vente();
-				vente.setNoVente(rs.getInt(16));
-				vente.setNomArticle(rs.getString(17));
-				vente.setDescription(rs.getString(18));
-				vente.setDateFinEncheres(rs.getTimestamp(19));
-				vente.setMiseAPrix(rs.getInt(20));
-				vente.setPrixVente(rs.getInt(21));
-				
-				Categorie categorie = new Categorie();
-				categorie.setNoCategorie(rs.getInt(24));
-				categorie.setLibelle(rs.getString(25));
-				
-				Utilisateur vendeur = new Utilisateur();
-				vendeur.setNoUtilisateur(rs.getInt(26));
-				vendeur.setPseudo(rs.getString(27));
-				vendeur.setNom(rs.getString(28));
-				vendeur.setPrenom(rs.getString(29));
-				vendeur.setEmail(rs.getString(30));
-				vendeur.setTelephone(rs.getString(31));
-				vendeur.setRue(rs.getString(32));
-				vendeur.setCodePostal(rs.getString(33));
-				vendeur.setVille(rs.getString(34));
-				vendeur.setMotDePasse(rs.getString(35));
-				vendeur.setCredit(rs.getInt(36));
-				vendeur.setAdministrateur(rs.getBoolean(37));
-				
-				vente.setUtilisateurVendeur(vendeur);
-				vente.setUtilisateurAcheteur(acheteur);
-				vente.setCategorie(categorie);
-				
-				enchere.setUtilisateurEnchere(acheteur);
-				enchere.setDateEnchere(rs.getTimestamp(1));
-				enchere.setVenteEnchere(vente);
-				
-//				System.out.println("select Enchere: " + enchere.toString());
-			}
-			rs.close();
-			pstmt.close();
+		try(Connection con = ConnectionProvider.openConnection()) {
+			try(PreparedStatement pstmt = con.prepareStatement(SELECT_BY_ID, PreparedStatement.RETURN_GENERATED_KEYS)){
+				pstmt.setInt(1, noUtlisateur);
+				pstmt.setInt(2, noVente);
+				try(ResultSet rs = pstmt.executeQuery()){
+					if (rs.next()) {		
+						
+						Utilisateur acheteur = new Utilisateur();
+						acheteur.setNoUtilisateur(rs.getInt(4));
+						acheteur.setPseudo(rs.getString(5));
+						acheteur.setNom(rs.getString(6));
+						acheteur.setPrenom(rs.getString(7));
+						acheteur.setEmail(rs.getString(8));
+						acheteur.setTelephone(rs.getString(9));
+						acheteur.setRue(rs.getString(10));
+						acheteur.setCodePostal(rs.getString(11));
+						acheteur.setVille(rs.getString(12));
+						acheteur.setMotDePasse(rs.getString(13));
+						acheteur.setCredit(rs.getInt(14));
+						acheteur.setAdministrateur(rs.getBoolean(15));
+						
+						Vente vente = new Vente();
+						vente.setNoVente(rs.getInt(16));
+						vente.setNomArticle(rs.getString(17));
+						vente.setDescription(rs.getString(18));
+						vente.setDateFinEncheres(rs.getTimestamp(19));
+						vente.setMiseAPrix(rs.getInt(20));
+						vente.setPrixVente(rs.getInt(21));
+						
+						Categorie categorie = new Categorie();
+						categorie.setNoCategorie(rs.getInt(24));
+						categorie.setLibelle(rs.getString(25));
+						
+						Utilisateur vendeur = new Utilisateur();
+						vendeur.setNoUtilisateur(rs.getInt(26));
+						vendeur.setPseudo(rs.getString(27));
+						vendeur.setNom(rs.getString(28));
+						vendeur.setPrenom(rs.getString(29));
+						vendeur.setEmail(rs.getString(30));
+						vendeur.setTelephone(rs.getString(31));
+						vendeur.setRue(rs.getString(32));
+						vendeur.setCodePostal(rs.getString(33));
+						vendeur.setVille(rs.getString(34));
+						vendeur.setMotDePasse(rs.getString(35));
+						vendeur.setCredit(rs.getInt(36));
+						vendeur.setAdministrateur(rs.getBoolean(37));
+						
+						vente.setUtilisateurVendeur(vendeur);
+						vente.setUtilisateurAcheteur(acheteur);
+						vente.setCategorie(categorie);
+						
+						enchere.setUtilisateurEnchere(acheteur);
+						enchere.setDateEnchere(rs.getTimestamp(1));
+						enchere.setVenteEnchere(vente);
+						
+//						System.out.println("select Enchere: " + enchere.toString());
+					}
+				}
+			}		
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			con = ConnectionProvider.closeConnection();		
+			throw new DALException("Erreur select");
 		}
 		return enchere;
 	}
 
 	@Override
 	public List<Enchere> selectAll() throws DALException {
-		Connection con = ConnectionProvider.openConnection();
 		List<Enchere> listEnchere = new ArrayList<Enchere>();
-		try {
-			PreparedStatement pstmt = con.prepareStatement(SELECT_ALL, PreparedStatement.RETURN_GENERATED_KEYS);
-			ResultSet rs = pstmt.executeQuery();
-	        while (rs.next()) {
-	    		Enchere enchere = new Enchere();
-	    		
-	        	Utilisateur acheteur = new Utilisateur();
-				acheteur.setNoUtilisateur(rs.getInt(4));
-				acheteur.setPseudo(rs.getString(5));
-				acheteur.setNom(rs.getString(6));
-				acheteur.setPrenom(rs.getString(7));
-				acheteur.setEmail(rs.getString(8));
-				acheteur.setTelephone(rs.getString(9));
-				acheteur.setRue(rs.getString(10));
-				acheteur.setCodePostal(rs.getString(11));
-				acheteur.setVille(rs.getString(12));
-				acheteur.setMotDePasse(rs.getString(13));
-				acheteur.setCredit(rs.getInt(14));
-				acheteur.setAdministrateur(rs.getBoolean(15));
-				
-				Vente vente = new Vente();
-				vente.setNoVente(rs.getInt(16));
-				vente.setNomArticle(rs.getString(17));
-				vente.setDescription(rs.getString(18));
-				vente.setDateFinEncheres(rs.getTimestamp(19));
-				vente.setMiseAPrix(rs.getInt(20));
-				vente.setPrixVente(rs.getInt(21));
-				
-				Categorie categorie = new Categorie();
-				categorie.setNoCategorie(rs.getInt(24));
-				categorie.setLibelle(rs.getString(25));
-				
-				Utilisateur vendeur = new Utilisateur();
-				vendeur.setNoUtilisateur(rs.getInt(26));
-				vendeur.setPseudo(rs.getString(27));
-				vendeur.setNom(rs.getString(28));
-				vendeur.setPrenom(rs.getString(29));
-				vendeur.setEmail(rs.getString(30));
-				vendeur.setTelephone(rs.getString(31));
-				vendeur.setRue(rs.getString(32));
-				vendeur.setCodePostal(rs.getString(33));
-				vendeur.setVille(rs.getString(34));
-				vendeur.setMotDePasse(rs.getString(35));
-				vendeur.setCredit(rs.getInt(36));
-				vendeur.setAdministrateur(rs.getBoolean(37));
-				
-				vente.setUtilisateurVendeur(vendeur);
-				vente.setUtilisateurAcheteur(acheteur);
-				vente.setCategorie(categorie);
-				
-				enchere.setUtilisateurEnchere(acheteur);
-				enchere.setDateEnchere(rs.getTimestamp(1));
-				enchere.setVenteEnchere(vente);
-				
-				listEnchere.add(enchere);
-				
-//				System.out.println("Categorie: " + enchere.toString());
+		try(Connection con = ConnectionProvider.openConnection()) {
+			try(PreparedStatement pstmt = con.prepareStatement(SELECT_ALL, PreparedStatement.RETURN_GENERATED_KEYS)){
+				try(ResultSet rs = pstmt.executeQuery()){
+					while (rs.next()) {
+			    		Enchere enchere = new Enchere();
+			    		
+			        	Utilisateur acheteur = new Utilisateur();
+						acheteur.setNoUtilisateur(rs.getInt(4));
+						acheteur.setPseudo(rs.getString(5));
+						acheteur.setNom(rs.getString(6));
+						acheteur.setPrenom(rs.getString(7));
+						acheteur.setEmail(rs.getString(8));
+						acheteur.setTelephone(rs.getString(9));
+						acheteur.setRue(rs.getString(10));
+						acheteur.setCodePostal(rs.getString(11));
+						acheteur.setVille(rs.getString(12));
+						acheteur.setMotDePasse(rs.getString(13));
+						acheteur.setCredit(rs.getInt(14));
+						acheteur.setAdministrateur(rs.getBoolean(15));
+						
+						Vente vente = new Vente();
+						vente.setNoVente(rs.getInt(16));
+						vente.setNomArticle(rs.getString(17));
+						vente.setDescription(rs.getString(18));
+						vente.setDateFinEncheres(rs.getTimestamp(19));
+						vente.setMiseAPrix(rs.getInt(20));
+						vente.setPrixVente(rs.getInt(21));
+						
+						Categorie categorie = new Categorie();
+						categorie.setNoCategorie(rs.getInt(24));
+						categorie.setLibelle(rs.getString(25));
+						
+						Utilisateur vendeur = new Utilisateur();
+						vendeur.setNoUtilisateur(rs.getInt(26));
+						vendeur.setPseudo(rs.getString(27));
+						vendeur.setNom(rs.getString(28));
+						vendeur.setPrenom(rs.getString(29));
+						vendeur.setEmail(rs.getString(30));
+						vendeur.setTelephone(rs.getString(31));
+						vendeur.setRue(rs.getString(32));
+						vendeur.setCodePostal(rs.getString(33));
+						vendeur.setVille(rs.getString(34));
+						vendeur.setMotDePasse(rs.getString(35));
+						vendeur.setCredit(rs.getInt(36));
+						vendeur.setAdministrateur(rs.getBoolean(37));
+						
+						vente.setUtilisateurVendeur(vendeur);
+						vente.setUtilisateurAcheteur(acheteur);
+						vente.setCategorie(categorie);
+						
+						enchere.setUtilisateurEnchere(acheteur);
+						enchere.setDateEnchere(rs.getTimestamp(1));
+						enchere.setVenteEnchere(vente);
+						
+						listEnchere.add(enchere);
+						
+//						System.out.println("Categorie: " + enchere.toString());
+					}
+				}
 			}
-	        rs.close();
-	        pstmt.close();
 		}catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			con = ConnectionProvider.closeConnection();		
+			throw new DALException("Erreur selectAll");
 		}
 		return listEnchere;
 	}
