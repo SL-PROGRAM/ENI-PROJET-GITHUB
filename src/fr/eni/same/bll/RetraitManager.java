f6bff3b32dd64e01a50500e4b212e206fe0c26e5
sl <s.leyral@gmail.com>
2020-05-18 11:35:09 +0200

    suppression adresseManager

--- src/fr/eni/same/bll/RetraitManager.java
+++ src/fr/eni/same/bll/RetraitManager.java
@@ -6,7 +6,7 @@ import fr.eni.same.bll.interfaceManager.RetraitManagerInterface;
 import fr.eni.same.bo.Retrait;
 import fr.eni.same.exception.BllException;
 
-public class RetraitManager extends AdresseManager implements RetraitManagerInterface {
+public class RetraitManager implements RetraitManagerInterface {
 	
 	@Override
 	public void insert(Retrait t) throws BllException {
@@ -36,6 +36,24 @@ public class RetraitManager extends AdresseManager implements RetraitManagerInte
 	public List<Retrait> selectAll() throws BllException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean isRueLongueurCorrect(String libelle) throws BllException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCodePostalLongueurCorrect(String libelle) throws BllException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isVilleLongueurCorrect(String libelle) throws BllException {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 
