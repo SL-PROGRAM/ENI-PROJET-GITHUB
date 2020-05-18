2dbdea46a44a96ea7fa648173476f24c1a1adff8
sl <s.leyral@gmail.com>
2020-05-18 14:47:23 +0200

    ajout methode filtre

--- src/fr/eni/same/bll/filtreManager.java
+++ src/fr/eni/same/bll/filtreManager.java
@@ -1,17 +1,16 @@
 package fr.eni.same.bll;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import fr.eni.same.bo.Vente;
 
 public class filtreManager {
 	
-	public List<Vente> filtreMesVentes(){
-		List<Vente> mesVentes = new ArrayList<Vente>();
-		
-		
-		return mesVentes;
+	public List<Vente> filtreMesVentesPubliées(){
+		List<Vente> mesVentesPubliées = new ArrayList<Vente>();
+		return mesVentesPubliées;
 	}
 	
 	public List<Vente> filtreMesVentesEnregistrees(){
@@ -21,11 +20,27 @@ public class filtreManager {
 		return mesVentesEnregistrees;
 	}
 	
-	public List<Vente> filtreMesEncheresEnregistrees(){
-		List<Vente> mesEncheresEnregistrees = new ArrayList<Vente>();
+	public List<Vente> filtreMesEncheresEnCours(){
+		List<Vente> mesEncheresEnCours = new ArrayList<Vente>();
 		
 		
-		return mesEncheresEnregistrees;
+		return mesEncheresEnCours;
 	}
-
+	
+	public List<Vente> filtreMesAcquisitions(){
+		List<Vente> mesAcquisitions = new ArrayList<Vente>();
+		
+		
+		return mesAcquisitions;
+	}
+	
+	public List<Vente> filtreAutresEncheres(){
+		List<Vente> autresEncheres = new ArrayList<Vente>();
+		
+		
+		return autresEncheres;
+	}
+	
+	
+	
 }
