70ea388de06f3d71354e67d61fc4ce199d4dde59
Etienne <etienne.fdarcier@hotmail.fr>
2020-05-18 14:55:13 +0200

    Correction return msgErreur

--- src/fr/eni/same/helpers/AdresseUtils.java
+++ src/fr/eni/same/helpers/AdresseUtils.java
@@ -12,34 +12,37 @@ public abstract class AdresseUtils  {
 	
 
 	public static String rueLongueurCorrect(String libelle) throws BllException {
+		String msgErreur = "";
 		if(!FonctionGenerique.isLongueurMax(libelle, RUE_LONGUEUR_MAX)) {
-			throw new BllException("Longueur de la rue est trop importante - Longueur maximum : "+ RUE_LONGUEUR_MAX + " longueur actuelle : " + libelle.length());
+			msgErreur += "Longueur de la rue est trop importante - Longueur maximum : "+ RUE_LONGUEUR_MAX + " longueur actuelle : " + libelle.length();
 		}
 		if(!FonctionGenerique.isLongueurMin(libelle, RUE_LONGUEUR_MIN)) {
-			throw new BllException("Longueur de la rue est trop courte - Longueur minimum : "+ RUE_LONGUEUR_MIN);
+			msgErreur  += "Longueur de la rue est trop courte - Longueur minimum : "+ RUE_LONGUEUR_MIN;
 		}
-		return libelle;				
+		return msgErreur ;				
 	}
 	
 
 	public static String codePostalLongueurCorrect(String libelle) throws BllException {
+		String msgErreur = "";
 		if(!FonctionGenerique.isLongueurMax(libelle, CP_LONGUEUR_MAX)) {
-			throw new BllException("Longueur du code postal trop importante - Longueur maximum : "+ CP_LONGUEUR_MAX);
+			msgErreur += "Longueur du code postal trop importante - Longueur maximum : "+ CP_LONGUEUR_MAX + " taille actuelle : " + libelle;
 		}
 		if(!FonctionGenerique.isLongueurMin(libelle, CP_LONGUEUR_MIN)) {
-			throw new BllException("Longueur code postal trop courte - Longueur minimum : "+ CP_LONGUEUR_MIN);
+			msgErreur += "Longueur code postal trop courte - Longueur minimum : "+ CP_LONGUEUR_MIN;
 		}
-		return libelle;				
+		return msgErreur;				
 	}
 
 	public static String villeLongueurCorrect(String libelle) throws BllException {
+		String msgErreur = "";
 		if(!FonctionGenerique.isLongueurMax(libelle, VILLE_LONGUEUR_MAX)) {
-			throw new BllException("Longueur de la ville trop importante - Longueur 	maximum : "+ VILLE_LONGUEUR_MAX);
+			msgErreur = "Longueur de la ville trop importante - Longueur 	maximum : "+ VILLE_LONGUEUR_MAX;
 		}
 		if(!FonctionGenerique.isLongueurMin(libelle, VILLE_LONGUEUR_MIN)) {
-			throw new BllException("Longueur de la ville trop courte - Longueur minimum : "+ VILLE_LONGUEUR_MIN);
+			msgErreur += "Longueur de la ville trop courte - Longueur minimum : "+ VILLE_LONGUEUR_MIN;
 		}
-		return libelle;				
+		return msgErreur ;				
 	}
 	
 }
