dabf094841ff438c4cc8181b92629cb78fa5612e
sl <s.leyral@gmail.com>
2020-05-18 14:46:37 +0200

    Creation des filtres

--- src/fr/eni/same/bll/filtreManager.java
+++ src/fr/eni/same/bll/filtreManager.java
@@ -1,7 +1,31 @@
 package fr.eni.same.bll;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.same.bo.Vente;
+
 public class filtreManager {
 	
-	//Methode lier au filtre 
+	public List<Vente> filtreMesVentes(){
+		List<Vente> mesVentes = new ArrayList<Vente>();
+		
+		
+		return mesVentes;
+	}
+	
+	public List<Vente> filtreMesVentesEnregistrees(){
+		List<Vente> mesVentesEnregistrees = new ArrayList<Vente>();
+		
+		
+		return mesVentesEnregistrees;
+	}
+	
+	public List<Vente> filtreMesEncheresEnregistrees(){
+		List<Vente> mesEncheresEnregistrees = new ArrayList<Vente>();
+		
+		
+		return mesEncheresEnregistrees;
+	}
 
 }
