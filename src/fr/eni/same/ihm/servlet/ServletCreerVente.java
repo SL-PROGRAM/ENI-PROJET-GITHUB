604053fe61ae0070ed3ad6733722178ff58b0dc4
andrea <andrearaiatea@gmail.com>
2020-05-18 17:18:50 +0200

    recup adresse servletCreerVente
    recupération adressse pr affichage par défaut, debut recupération donnée formulaire

--- src/fr/eni/same/ihm/servlet/ServletCreerVente.java
+++ src/fr/eni/same/ihm/servlet/ServletCreerVente.java
@@ -1,6 +1,7 @@
 package fr.eni.same.ihm.servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.same.bo.Vente;
 
 /**
  * Servlet implementation class ServletCreerVente
@@ -26,6 +30,22 @@ public class ServletCreerVente extends HttpServlet {
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getSession().getAttribute("utilisateur") == null){
+		response.sendRedirect("connexion");
+    	return;
+	    }else{//recup info adresse de l utilisateur pour placer par defaut l adresse a l emplacement retrait
+	    	HttpSession session = request.getSession();
+			
+			String rue = (String) session.getAttribute("rue");
+			String codePostal = (String)session.getAttribute("codePostal");
+			String ville = (String)session.getAttribute("ville");
+			
+			
+			request.setAttribute("rue", rue);
+			request.setAttribute("codePostal", codePostal);
+			request.setAttribute("ville", ville);
+	    }
+			
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creerVentes.jsp");
 		rd.forward(request, response);
 	}
@@ -34,7 +54,21 @@ public class ServletCreerVente extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		Vente newVente= new Vente();
+		newVente.setNomArticle(request.getParameter("article"));
+		newVente.setDescription(request.getParameter("articleDescription"));
+		//newVente.setCategorie(request.getParameterValues("")); a chercher comment faire pour le select
+		//newVente.setDateFinEncheres(LocalDate.parse(request.getParameter("dateFinEnchere")));
+		//newVente.setMiseAPrix.parseInt(request.getParameter("miseAPrix"));
+		//newVente.setUtilisateurVendeur.(request.getParameter("rue"));
+		
+		if(request.getParameter("publierVente") != null) {
+			//vente.ajouterVente(newVente);
+		}else if(request.getParameter("enregistrerVente") != null) {
+			//cookie
+		}else if(request.getParameter("annuler") != null) {
+			//annuler enregistrement
+		}
 	}
 
 	
