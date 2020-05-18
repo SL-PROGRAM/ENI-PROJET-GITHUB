3f0627628eab15c5561d2e2516ec5d73edd5d4db
Etienne <etienne.fdarcier@hotmail.fr>
2020-05-18 14:32:24 +0200

    Filtre Manager créé

--- src/fr/eni/same/bll/filtreManager.java
+++ src/fr/eni/same/bll/filtreManager.java
@@ -1,7 +1,46 @@
 package fr.eni.same.bll;
 
-public class filtreManager {
-	
-	//Methode lier au filtre 
+import java.util.ArrayList;
+import java.util.List;
 
+import fr.eni.same.bo.Vente;
+import fr.eni.same.exception.BllException;
+
+//Class liée aux filtres
+public class FiltreManager {
+	
+	private static FiltreManager instance;
+	
+	private FiltreManager() {
+	}
+	
+	public static synchronized FiltreManager getFiltreManager() {
+		if(instance == null) {
+			instance = new FiltreManager();
+		}
+		return instance;
+	}
+	
+	public void getMesVentes() {
+		
+	}
+	public void getMesEncheresEnCours() {
+		
+	}
+	public void getMesAcquisitions() {
+		
+	}
+	public void getAutresEncheres() {
+		
+	}
+	public void getMesVentesEnregistrees() {
+		
+	}
+	/**
+	 * Recherche le nom d'un article correspondant aux X premières lettres dans le champ de recherche
+	 * @param input : le mot du champ de recherche
+	 */
+	public void getRechercheParMotCles() {
+		
+	}
 }
