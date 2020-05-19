b897cb3afa2abb1b7e3bfa84fbb96e037914a6aa
sl <s.leyral@gmail.com>
2020-05-19 17:31:58 +0200

    mise en place session DetailVente

--- src/fr/eni/same/ihm/servlet/ServletDetailVente.java
+++ src/fr/eni/same/ihm/servlet/ServletDetailVente.java
@@ -14,7 +14,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import fr.eni.same.bo.Utilisateur;
 import fr.eni.same.bo.Vente;
 
 /**
@@ -35,7 +37,19 @@ public class ServletDetailVente extends HttpServlet {
 	 *  Cette Servlet et la jsp correspondante prennent en charge les Maquettes 9 et 10
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		Vente vente = (Vente) request.getAttribute("vente");
+		HttpSession session = request.getSession();
+		
+		if(vente.getUtilisateurVendeur() == (Utilisateur) session.getAttribute("utilisateur")) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/detailVente.jsp");
+			rd.forward(request, response);
+		}else {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/pageEncherir.jsp");
+			rd.forward(request, response);
+		}
+		
+		
 		Timestamp dateFinEnchere = vente.getDateFinEncheres();
 		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		String heureServer = format.format(GregorianCalendar.getInstance().getTime());
