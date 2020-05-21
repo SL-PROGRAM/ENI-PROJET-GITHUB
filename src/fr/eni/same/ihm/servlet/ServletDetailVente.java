b3aa584bd19f8acfb04abdeebed828115f40e7c0
andrea <andrearaiatea@gmail.com>
2020-05-21 21:40:38 +0200

    servletDetailVente a jour

--- src/fr/eni/same/ihm/servlet/ServletDetailVente.java
+++ src/fr/eni/same/ihm/servlet/ServletDetailVente.java
@@ -66,22 +66,26 @@ public class ServletDetailVente extends HttpServlet {
 				Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
 				
 				if(vente.getDateFinEncheres().before(heureServer)  ) {
-					if(vente.getUtilisateurAcheteur().getNoUtilisateur() != utilisateur.getNoUtilisateur()) {
-						RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/detailVente.jsp");
-						rd.forward(request, response);
-					}
 					
-					
-					else if(vente.getUtilisateurAcheteur() != null 
+					if(vente.getUtilisateurAcheteur() != null 
 							&& vente.getUtilisateurAcheteur().getNoUtilisateur() == utilisateur.getNoUtilisateur()) {
 						RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/enchereGagnee.jsp");
-					rd.forward(request, response);
-					}else {
+					rd.forward(request, response);	
+				
+				
+					}
+					
+					else if (vente.getUtilisateurAcheteur() != null 
+							&& vente.getUtilisateurVendeur().getNoUtilisateur() != utilisateur.getNoUtilisateur()){
 						String msgErreur = "Cette vente est terminée, vous n'êtes pas le grand gagnant désolé";
 						request.setAttribute("erreur", msgErreur);
 						RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/listeEnchere.jsp");
 						rd.forward(request, response);
-					}
+					
+					}else if(vente.getUtilisateurAcheteur().getNoUtilisateur() != utilisateur.getNoUtilisateur()) {
+							RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/detailVente.jsp");
+							rd.forward(request, response);
+						}
 					
 				}
 		
